715ff7f8a2e3f51801e0a108a62c8e872ae635e4
hiago dos santos <hiagop22@gmail.com>
2018-08-16 19:40:54 -0300

    inserted the communication with ros and finish tested. All things seem working

--- communication_ros.py
+++ communication_ros.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python3
 
 import rospy
-#from python_simulator.msg import robots_speeds_msg
-#from msg import wheels_speeds_msg
-#from msg import VisionMessage
-
-### Para teste ### 
-from std_msgs.msg import Float32
-saida = Float32()
-
+from python_simulator.msg import robots_speeds_msg
+from python_simulator.msg import wheels_speeds_msg
+from python_simulator.msg import VisionMessage
 
 def add_communication_with_system(cls):
     """Class decorator function 
@@ -16,25 +11,28 @@ def add_communication_with_system(cls):
     class Run_ros(cls):
         def __init__(self, *arg, **kargs):
             try:
-                cls.__init__(self, *arg, **kargs)            
+                super().__init__(*arg, **kargs)            
                 self.run_communication()
             except rospy.ROSInterruptException:
                 pass
 
         def run_communication(self):
             rospy.init_node('Simulator')
-            # self.pub1 = rospy.Publisher('vision_output_topic', VisionMessage, queue_size=1)
-            # rospy.Subscriber('radio_topic',wheels_speeds_msg,self.update_speeds)
-            self.pub1 = rospy.Publisher('robots_speeds', Float32, queue_size=1)
+            self.pub1 = rospy.Publisher('vision_output_topic', VisionMessage, queue_size=1)
+            rospy.Subscriber('radio_topic', wheels_speeds_msg, self.update_speeds)
 
             #rate = rospy.Rate(30)
-            self.j = 0
+            self.j = VisionMessage()
+            self.k = [0, 0, 0, 0, 0, 0]
             self.iterador()   
 
         def iterador(self):
                 if rospy.is_shutdown():               # Finish the communication if ctrl+c
                     return                            # is pressed
-                self.j += 1
+
+                self.k[0] += 1.0
+                self.j.x = self.k
+
                 self.frame.after(2000, self.iterador) # 2000 milliseconds = 2 seconds
                 self.pub1.publish(self.j)
 
