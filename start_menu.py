86b3d683109aeb3be6643cd986903d82f07c7b55
Hiago dos Santos Rabelo <hiagop22@gmail.com>
2018-12-24 10:57:56 -0200

    precisa fazer o menu principal

--- start_menu.py
+++ start_menu.py
@@ -2,9 +2,252 @@
 #-*- coding: utf-8 -*-
 """
     @author: Hiago dos Santos (hiagop22@gmail.com)
-    @description: This module describes the Graphical user Interface(GUI) 
-    of UnBall simulator and how it works
+    @description: This is the main-menu 
+    Based on Pablo Pizarro R. pygame-menu:
+    https://github.com/ppizarror/pygame-menu/blob/master/example2.py
+
+The MIT License (MIT)
+Copyright 2017-2018 Pablo Pizarro R. @ppizarror
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
+
+# Import pygame and libraries
+from pygame.locals import *
+from random import randrange
+import os
+import pygame
+
+# Import pygameMenu
+import pygameMenu
+from pygameMenu.locals import *
+
+#ABOUT = ['PygameMenu {0}'.format(pygameMenu.__version__),
+#         'Author: {0}'.format(pygameMenu.__author__),
+#         PYGAMEMENU_TEXT_NEWLINE,
+#         'Email: {0}'.format(pygameMenu.__email__)]
+ABOUT = ['Python Simulation',
+         'Author: Hiago dos Santos Rabelo',
+         PYGAMEMENU_TEXT_NEWLINE,
+         'Email: hiagop22@gmail.com']
+
+
+COLOR_BACKGROUND = (3, 63, 118) 
+COLOR_BLACK = (0, 0, 0)
+COLOR_WHITE = (255, 255, 255)
+FPS = 60.0
+MENU_BACKGROUND_COLOR = (228, 55, 36)
+WINDOW_SIZE = (640, 480)
+
+# -----------------------------------------------------------------------------
+# Init pygame
+pygame.init()
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+# Create pygame screen and objects
+surface = pygame.display.set_mode(WINDOW_SIZE)
+pygame.display.set_caption('Python Simulation')
+clock = pygame.time.Clock()
+dt = 1 / FPS
+
+# Global variables
+DIFFICULTY = ['EASY']
+
+
+# -----------------------------------------------------------------------------
+
+def change_difficulty(d):
+    """
+    Change difficulty of the game.
+    
+    :return: 
+    """
+    print ('Selected difficulty: {0}'.format(d))
+    DIFFICULTY[0] = d
+
+
+def random_color():
+    """
+    Return random color.
+    
+    :return: Color tuple
+    """
+    return randrange(0, 255), randrange(0, 255), randrange(0, 255)
+
+
+def play_function(difficulty, font):
+    """
+    Main game function
+    
+    :param difficulty: Difficulty of the game
+    :param font: Pygame font
+    :return: None
+    """
+    difficulty = difficulty[0]
+    assert isinstance(difficulty, str)
+
+    if difficulty == 'EASY':
+        f = font.render('Playing as baby', 1, COLOR_WHITE)
+    elif difficulty == 'MEDIUM':
+        f = font.render('Playing as normie', 1, COLOR_WHITE)
+    elif difficulty == 'HARD':
+        f = font.render('Playing as god', 1, COLOR_WHITE)
+    else:
+        raise Exception('Unknown difficulty {0}'.format(difficulty))
+
+    # Draw random color and text
+    bg_color = random_color()
+    f_width = f.get_size()[0]
+
+    # Reset main menu and disable
+    # You also can set another menu, like a 'pause menu', or just use the same
+    # main_menu as the menu that will check all your input.
+    main_menu.disable()
+    main_menu.reset(1)
+
+    while True:
+
+        # Clock tick
+        clock.tick(60)
+
+        # Application events
+        playevents = pygame.event.get()
+        for e in playevents:
+            if e.type == QUIT:
+                exit()
+            elif e.type == KEYDOWN:
+                if e.key == K_ESCAPE and main_menu.is_disabled():
+                    main_menu.enable()
+
+                    # Quit this function, then skip to loop of main-menu on line 217
+                    return
+
+        # Pass events to main_menu
+        main_menu.mainloop(playevents)
+
+        # Continue playing
+        surface.fill(bg_color)
+        surface.blit(f, ((WINDOW_SIZE[0] - f_width) / 2, WINDOW_SIZE[1] / 2))
+        pygame.display.flip()
+
+
+def main_background():
+    """
+    Function used by menus, draw on background while menu is active.
+    
+    :return: None
+    """
+    surface.fill(COLOR_BACKGROUND)
+
+
+# -----------------------------------------------------------------------------
+# PLAY MENU
+play_menu = pygameMenu.Menu(surface,
+                            bgfun=main_background,
+                            color_selected=COLOR_WHITE,
+                            font=pygameMenu.fonts.FONT_BEBAS,
+                            font_color=COLOR_BLACK,
+                            font_size=30,
+                            menu_alpha=100,
+                            menu_color=MENU_BACKGROUND_COLOR,
+                            menu_height=int(WINDOW_SIZE[1] * 0.6),
+                            menu_width=int(WINDOW_SIZE[0] * 0.6),
+                            onclose=PYGAME_MENU_DISABLE_CLOSE,
+                            option_shadow=False,
+                            title='Play menu',
+                            window_height=WINDOW_SIZE[1],
+                            window_width=WINDOW_SIZE[0]
+                            )
+# When pressing return -> play(DIFFICULTY[0], font)
+play_menu.add_option('Start', play_function, DIFFICULTY,
+                     pygame.font.Font(pygameMenu.fonts.FONT_FRANCHISE, 30))
+play_menu.add_selector('Select difficulty', [('Easy', 'EASY'),
+                                             ('Medium', 'MEDIUM'),
+                                             ('Hard', 'HARD')],
+                       onreturn=None,
+                       onchange=change_difficulty)
+play_menu.add_option('Return to main menu', PYGAME_MENU_BACK)
+
+# ABOUT MENU
+about_menu = pygameMenu.TextMenu(surface,
+                                 bgfun=main_background,
+                                 color_selected=COLOR_WHITE,
+                                 font=pygameMenu.fonts.FONT_BEBAS,
+                                 font_color=COLOR_BLACK,
+                                 font_size_title=30,
+                                 font_title=pygameMenu.fonts.FONT_8BIT,
+                                 menu_color=MENU_BACKGROUND_COLOR,
+                                 menu_color_title=COLOR_WHITE,
+                                 menu_height=int(WINDOW_SIZE[1] * 0.6),
+                                 menu_width=int(WINDOW_SIZE[0] * 0.6),
+                                 onclose=PYGAME_MENU_DISABLE_CLOSE,
+                                 option_shadow=False,
+                                 text_color=COLOR_BLACK,
+                                 text_fontsize=20,
+                                 title='About',
+                                 window_height=WINDOW_SIZE[1],
+                                 window_width=WINDOW_SIZE[0]
+                                 )
+for m in ABOUT:
+    about_menu.add_line(m)
+about_menu.add_line(PYGAMEMENU_TEXT_NEWLINE)
+about_menu.add_option('Return to menu', PYGAME_MENU_BACK)
+
+# MAIN MENU
+main_menu = pygameMenu.Menu(surface,
+                            bgfun=main_background,
+                            color_selected=COLOR_WHITE,
+                            font=pygameMenu.fonts.FONT_BEBAS,
+                            font_color=COLOR_BLACK,
+                            font_size=30,
+                            menu_alpha=100,
+                            menu_color=MENU_BACKGROUND_COLOR,
+                            menu_height=int(WINDOW_SIZE[1] * 0.6),
+                            menu_width=int(WINDOW_SIZE[0] * 0.6),
+                            onclose=PYGAME_MENU_DISABLE_CLOSE,
+                            option_shadow=False,
+                            title='Main menu',
+                            window_height=WINDOW_SIZE[1],
+                            window_width=WINDOW_SIZE[0]
+                            )
+main_menu.add_option('Play', play_menu)
+main_menu.add_option('About', about_menu)
+main_menu.add_option('Quit', PYGAME_MENU_EXIT)
+
+# -----------------------------------------------------------------------------
+# Main loop
+while True:
+
+    # Tick
+    clock.tick(60)
+
+    # Application events
+    events = pygame.event.get()
+    for event in events:
+        if event.type == QUIT:
+            exit()
+
+    # Main menu
+    main_menu.mainloop(events)
+
+    # Flip surface
+    pygame.display.flip()
+
 
 #from resolution import *
 import os
