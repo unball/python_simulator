4a0149dfb8b1d32f642be69486a29628784175d8
Igor Beduin <igor.rob.96@gmail.com>
2019-02-11 16:56:50 -0200

    Changing vision message package from "simulator" to "vision"

--- communication_ros.py
+++ communication_ros.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 #   -*- coding: utf-8 -*-
 #      @author: Hiago dos Santos Rabelo (hiagop22@gmail.com)
-# @description: This module run the Ros publishing in node chosen on start_menu 
+# @description: This module run the Ros publishing in node chosen on start_menu
 #               and subscribing from radio_topic.
 
 
 import rospy
 from constants import *
 from python_simulator.msg import comm_msg
-from python_simulator.msg import VisionMessage
-from objects_on_field.physics_engine import (motor_voltage_to_wheels_speed, 
+from vision.msg import VisionMessage
+from objects_on_field.physics_engine import (motor_voltage_to_wheels_speed,
                                             wheels_speeds_to_robots_speeds)
 											# ,actual_axis_to_axis_unball)
 
@@ -30,13 +30,13 @@ class RunRos(object):
 
         self.sub = rospy.Subscriber('radio_topic', comm_msg, self.callback)
 
-        self.num_allies = 0    
+        self.num_allies = 0
         self.num_opponents = 0
 
     def callback(self, data):
-        wheels = [motor_voltage_to_wheels_speed(data.MotorA[x], data.MotorB[x]) 
+        wheels = [motor_voltage_to_wheels_speed(data.MotorA[x], data.MotorB[x])
                                                      for x in range(self.num_allies)]
-        self.ang_and_lin_speed = [wheels_speeds_to_robots_speeds(wheels[x][0], wheels[x][1]) 
+        self.ang_and_lin_speed = [wheels_speeds_to_robots_speeds(wheels[x][0], wheels[x][1])
                                                                 for x in range(self.num_allies)]
 
     def update(self, pos_robots_allies, pos_robots_opponents, pos_ball):
@@ -44,8 +44,8 @@ class RunRos(object):
         self.pub.publish(self.vision_message)
 
     def update_vision_message(self, pos_robots_allies, pos_robots_opponents, pos_ball):
-        # pos_robots_allies, pos_robots_opponents, pos_ball = actual_axis_to_axis_unball(pos_robots_allies, 
-                                                                                       # pos_robots_opponents, 
+        # pos_robots_allies, pos_robots_opponents, pos_ball = actual_axis_to_axis_unball(pos_robots_allies,
+                                                                                       # pos_robots_opponents,
                                                                                        # pos_ball)
 
         for x in range(self.num_allies):
@@ -59,7 +59,7 @@ class RunRos(object):
             self.vision_message.y[x + MAX_ROBOTS_ALLIES] = pos_robots_opponents[x][0][1]
             self.vision_message.th[x + MAX_ROBOTS_ALLIES] = pos_robots_opponents[x][1]
             self.vision_message.found[x + MAX_ROBOTS_ALLIES] = True
-        
+
         self.vision_message.ball_x = pos_ball[0]
         self.vision_message.ball_y = pos_ball[1]
 
