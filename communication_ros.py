5d4d414d55490551d1df81496ba373b8a7b02624
hiago dos santos rabelo <hiagop22@gmail.com>
2019-02-21 00:12:35 -0300

    saida do simulador de centimetros para metros, mudança de mensagem de subscribe...agora é robots_speeds_msg de communication.msg

--- communication_ros.py
+++ communication_ros.py
@@ -4,15 +4,10 @@
 # @description: This module run the Ros publishing in node chosen on start_menu
 #               and subscribing from radio_topic.
 
-
 import rospy
 from constants import *
-from python_simulator.msg import comm_msg
+from communication.msg import robots_speeds_msg
 from vision.msg import VisionMessage
-from objects_on_field.physics_engine import (motor_voltage_to_wheels_speed,
-                                            wheels_speeds_to_robots_speeds)
-											# ,actual_axis_to_axis_unball)
-
 
 class RunRos(object):
     def __init__(self, publish_topic):
@@ -28,39 +23,37 @@ class RunRos(object):
             pass # Inserir aqui o outro nó para ser publicado caso seja mudado no menu inicial
                  # Obs.: é necessário inserir a outra opção de nó tanto aqui quanto no menu inicial
 
-        self.sub = rospy.Subscriber('radio_topic', comm_msg, self.callback)
+        self.sub = rospy.Subscriber('radio_topic', robots_speeds_msg, self.callback)
 
         self.num_allies = 0
         self.num_opponents = 0
 
     def callback(self, data):
-        wheels = [motor_voltage_to_wheels_speed(data.MotorA[x], data.MotorB[x])
-                                                     for x in range(self.num_allies)]
-        self.ang_and_lin_speed = [wheels_speeds_to_robots_speeds(wheels[x][0], wheels[x][1])
-                                                                for x in range(self.num_allies)]
+        # wheels = [motor_voltage_to_wheels_speed(data.MotorA[x], data.MotorB[x])
+                                                     # for x in range(self.num_allies)]
+        self.ang_and_lin_speed = [(data.angular_vel[x],
+                                  data.linear_vel[x]*CORRECTION_FATOR_METER_TO_CM)
+                                  for x in range(self.num_allies)]
 
     def update(self, pos_robots_allies, pos_robots_opponents, pos_ball):
         self.update_vision_message(pos_robots_allies, pos_robots_opponents, pos_ball)
         self.pub.publish(self.vision_message)
 
-    def update_vision_message(self, pos_robots_allies, pos_robots_opponents, pos_ball):
-        # pos_robots_allies, pos_robots_opponents, pos_ball = actual_axis_to_axis_unball(pos_robots_allies,
-                                                                                       # pos_robots_opponents,
-                                                                                       # pos_ball)
+    def update_vision_message(self, pos_robots_allies, pos_robots_opponents, pos_ball):                                                                                       # pos_ball)
 
         for x in range(self.num_allies):
-            self.vision_message.x[x] = pos_robots_allies[x][0][0]
-            self.vision_message.y[x] = pos_robots_allies[x][0][1]
+            self.vision_message.x[x] = pos_robots_allies[x][0][0] * (10**-2)
+            self.vision_message.y[x] = pos_robots_allies[x][0][1] * (10**-2)
             self.vision_message.th[x] = pos_robots_allies[x][1]
             self.vision_message.found[x] = True
 
         for x in range(self.num_opponents):
-            self.vision_message.x[x + MAX_ROBOTS_ALLIES] = pos_robots_opponents[x][0][0]
-            self.vision_message.y[x + MAX_ROBOTS_ALLIES] = pos_robots_opponents[x][0][1]
+            self.vision_message.x[x + MAX_ROBOTS_ALLIES] = pos_robots_opponents[x][0][0] * (10**-2)
+            self.vision_message.y[x + MAX_ROBOTS_ALLIES] = pos_robots_opponents[x][0][1] * (10**-2)
             self.vision_message.th[x + MAX_ROBOTS_ALLIES] = pos_robots_opponents[x][1]
             self.vision_message.found[x + MAX_ROBOTS_ALLIES] = True
 
-        self.vision_message.ball_x = pos_ball[0]
-        self.vision_message.ball_y = pos_ball[1]
+        self.vision_message.ball_x = pos_ball[0] * (10**-2)
+        self.vision_message.ball_y = pos_ball[1] * (10**-2)
 
         print(self.vision_message)
