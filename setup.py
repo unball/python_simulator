fcf345dcb10e4ebcea91b262c3ee3f7a07ffaf8e
Hiago dos Santos <hiagop22@gmail.com>
2021-12-10 09:42:55 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Hiago dos Santos",
     author_email="hiagop22@gmail.com",
     url="https://unball.github.io/",
-    packages=["PythonSimulator", "PythonSimulator.field.Field"],
+    packages=["PythonSimulator", "PythonSimulator.field"],
     install_requires=[
         "box2d>=2.3.10",
         "pygame>=2.0.0",
