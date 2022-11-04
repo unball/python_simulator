d3329956775c4ee7a846523b3233cd9b8b6e256b
Hiago dos Santos <hiagop22@gmail.com>
2022-11-04 15:08:30 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name="PythonSimulator",
-    version="0.1.1",
+    version="0.1.2",
     description="UnBall-IA Simulação de campo.",
     author="Hiago dos Santos",
     author_email="hiagop22@gmail.com",
     url="https://unball.github.io/",
     packages=["PythonSimulator", "PythonSimulator.objects_on_field", "PythonSimulator.pygame_framework", "PythonSimulator.pygame_framework.backends"],
     install_requires=[
-        "box2d>=2.3.10",
+        "box2d>=2.3.2",
         "pygame>=2.0.0",
     ],
 )
